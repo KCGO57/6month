9276f3d64e9ea830ab1a8a09683df01407403cf1
KCGO57 <kjc0765@gmail.com>
2026-09-09 13:04:30 -0700

    .

---
+++ index.js
@@ -0,0 +1,88 @@
+
+const start = document.getElementById("start");
+const border = document.getElementById("border");
+const shoe = document.getElementById("shoe");
+const page_one = document.getElementById("page_one");
+
+const one = document.getElementById("one");
+const letter = document.getElementById("letter");
+
+const memories = document.getElementById("memories");
+
+const song = document.getElementById("song");
+
+const back = document.getElementById("back");
+const back2 = document.getElementById("back2");
+const back3 = document.getElementById("back3");
+
+start.addEventListener('click', () =>{
+    start.style.display = "none";
+    page_one.style.display = "flex";
+    song.loop = true;
+    song.play();
+    back.style.display = "block";
+    back2.style.display = "none";
+    back3.style.display = "none";
+});
+
+one.addEventListener('click', () =>{
+    page_one.style.display = "none";
+    letter.style.display = "flex";
+    border.style.display = "none";
+    document.body.style.backgroundImage = "url('pictures/date.jpg')";
+    document.body.style.backgroundSize = "1800px 800px";
+    document.body.style.backgroundAttachment = 'fixed';
+    back.style.display = "none";
+    back2.style.display = "block";
+    back3.style.display = "none";
+
+});
+
+
+two.addEventListener('click', () =>{
+    page_one.style.display = "none";
+    memories.style.display = "block";
+    document.body.style.overflow = 'auto';
+    border.style.display = "none";
+    document.body.style.backgroundImage = "url('pictures/shoe.JPG')";
+    document.body.style.backgroundSize = "1415px 900px";
+    document.body.style.backgroundAttachment = 'fixed';
+    back.style.display = "none";
+    back2.style.display = "none";
+    letter.style.display = "none";
+    back3.style.display = "block";
+
+});
+
+back.addEventListener('click', () =>{
+    start.style.display = "block";
+    page_one.style.display = "none";
+    back.style.display = "none";
+    back2.style.display = "none";
+    letter.style.display = "none";
+    back3.style.display = "none";
+});
+
+back2.addEventListener('click', () =>{
+    border.style.display = "block";
+    start.style.display = "none";
+    page_one.style.display = "flex";
+    back.style.display = "block";
+    back2.style.display = "none";
+    letter.style.display = "none";
+    document.body.style.backgroundImage = "none";
+    back3.style.display = "none";
+});
+
+back3.addEventListener('click', () =>{
+    border.style.display = "block";
+    start.style.display = "none";
+    page_one.style.display = "flex";
+    back.style.display = "block";
+    back2.style.display = "none";
+    letter.style.display = "none";
+    document.body.style.backgroundImage = "none";
+    back3.style.display = "none";
+    memories.style.display = "none";
+    document.body.style.overflow = 'hidden';
+});
